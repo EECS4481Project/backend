b602a83bcf2fe881b9781d54bbde309e6b15ea5b
DanteLaviolette <dante.laviolette@gmail.com>
2023-02-15 02:09:20 -0500

    prod db connection handling

--- src/db/db_factory.js
+++ src/db/db_factory.js
@@ -10,18 +10,28 @@ let connection = mongoose;
 
 if (isProd()) {
     // TODO: Connect to prod db
+    mongoose.connect(process.env.MONGO_DB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        auth: {
+            username: process.env.MONGO_USERNAME,
+            password: process.env.MONGO_PASSWORD,
+        },
+        dbName: "main"
+    });
 } else {
     // Connect to dev db
     mongoose.connect(process.env.MONGO_DB_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     });
-    const db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'failed to connect to db:'));
-    db.once('open', function () {
-        console.log('connected to mongodb');
-    });
 }
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'failed to connect to db:'));
+db.once('open', function () {
+    console.log('connected to mongodb');
+});
 
 // Add schemas to DB
 const agent = connection.model('Agent', agentSchema);
