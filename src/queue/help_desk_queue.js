2860f82f5b7ad5fa0b41ef04130add3aa4c42b2d
DanteLaviolette <dante.laviolette@gmail.com>
2023-02-17 02:49:35 -0500

    Expose queue to websockets

--- src/queue/help_desk_queue.js
+++ src/queue/help_desk_queue.js
@@ -1,9 +1,56 @@
 // Enqueues users & assigns them to an online help desk agent w/ availability
-const { server } = require('../server');
-const io = require("socket.io")(server, {
-    path: "/api/start_queue"
-});
+const { enqueue, pushToFrontOfQueue, userDisconnectedFromQueue } = require('./queue');
+const { io } = require('./queue_socketio');
+const { verifyAndParseFrontOfQueueToken } = require('./queue_token_utils');
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
+io.on('connection', async (socket) => {
+    // Disconnect if request is from an agent -- this is a user only service
+    if (socket.auth_token) {
+        socket.disconnect();
+        return;
+    }
+
+    socket.on('disconnect', () => {
+        if (socket.joinedQueue) {
+            userDisconnectedFromQueue(socket);
+        }
+    });
+
+    socket.on('ping', (msg) => {
+        socket.emit('pong');
+    })
+
+    socket.on('join_queue', async (msg) => {
+        if (typeof msg.token == 'string') {
+            // Bypass token case
+            const token = await verifyAndParseFrontOfQueueToken(msg.token);
+            if (token) {
+                // Use token info for socket params & push to front of queue
+                socket.userId = token.userId;
+                socket.firstName = token.firstName;
+                socket.lastName = token.lastName;
+                socket.joinedQueue = true;
+                pushToFrontOfQueue(socket);
+            } else {
+                // Invalid auth token
+                socket.emit('bad_auth', {});
+                socket.disconnect();
+            }
+        } else if (typeof msg.firstName == 'string' && typeof msg.lastName == 'string' && msg.firstName !== "" && msg.lastName !== "") {
+            // Non-empty first name & last name case. Push params to socket & enqueue the user
+            socket.firstName = msg.firstName;
+            socket.lastName = msg.lastName;
+            socket.joinedQueue = true;
+            enqueue(socket);
+        }
+        // If joining the queue failed, notify the user
+        if (!socket.joinedQueue) {
+            socket.emit('try_again', {});
+            socket.disconnect();
+        }
+    })
+
+    socket.on('test', msg => {
+        socket.emit('pong')
+    })
 });
---
+++ src/queue/queue_socketio.js
@@ -0,0 +1,22 @@
+// Creates the socket io object for the queue
+const cookieParser = require("cookie-parser");
+const { default: helmet } = require("helmet");
+const { populateAgentInSocket } = require("../auth/auth");
+const { server } = require('../server');
+
+const io = require("socket.io")(server, {
+    path: "/api/start_queue"
+});
+
+// Set secure default headers
+io.engine.use(helmet());
+// Parse cookies
+io.engine.use(cookieParser());
+
+// Populate agent if found (so we can kick them)
+io.use(populateAgentInSocket);
+// TODO: Add rate limiting
+
+module.exports = {
+    io
+}
