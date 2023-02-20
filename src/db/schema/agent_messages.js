ec85efec2b5114f91b85d086b779907ce7a7e272
DanteLaviolette <dante.laviolette@gmail.com>
2023-02-20 04:10:23 -0500

    Agent messages schema

---
+++ src/db/schema/agent_messages.js
@@ -0,0 +1,11 @@
+// Agent schema -- users who sign in/register.
+const mongoose = require('mongoose');
+
+const agentMessagesSchema = new mongoose.Schema({
+    senderUsername: { type: String, required: true },
+    receiverUsername: { type: String, required: true },
+    message: { type: String, required: true },
+    timestamp: { type: Number, required: true, index: true },
+});
+
+exports.agentMessagesSchema = agentMessagesSchema;
